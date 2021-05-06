0e8bb9f954470e0c4ffbeed118b68d8d67903e95
mnmndevlol <mnmndevlol@gmail.com>
2021-05-07 07:59:29 +0900

    9강 완

--- _pragmatic/urls.py
+++ _pragmatic/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-
-import accountapp
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- accountapp/views.py
+++ accountapp/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello_world(request): # 리퀘스트 필수인자이고 리퀘스트를받아서 리스폰스로 되돌려줌
-    return HttpResponse('Hello World!')
+    return render(request, 'accountapp/hello_world.html')
