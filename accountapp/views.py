7b01ef95ad78da979f87107a8882e7e8b993bd17
mnmndevlol <mnmndevlol@gmail.com>
2021-05-07 21:53:22 +0900

    16강 완

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def hello_world(request): # 리퀘스트 필수인자이고 리퀘스트를받아서 리스폰스로 되돌려줌
-    return render(request, 'accountapp/hello_world.html')
+def hello_world(request):
+    if request.method == "POST":
+        return render(request, 'accountapp/hello_world.html', context={'text':'POST METHOD!!!'})
+    else:
+        return render(request, 'accountapp/hello_world.html', context={'text':'GET METHOD!!!'})
