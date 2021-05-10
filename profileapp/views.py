bf33fefcdfb09d7faae0acb1b120a1cde605e633
mnmndevlol <mnmndevlol@gmail.com>
2021-05-11 00:01:13 +0900

    33강 완

--- profileapp/views.py
+++ profileapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, UpdateView
 
@@ -12,7 +12,6 @@ class ProfileCreateView(CreateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/create.html'
 
     def form_valid(self, form): #forms.py에서 리퀘스트로 날아온 폼 데이터가 여기 두번째인자의 form안에 있다.
@@ -21,7 +20,9 @@ class ProfileCreateView(CreateView):
         temp_profile.save() # 그렇게 합쳐진걸 최종적으로 저장함.
         return super().form_valid(form) #그리고 몇글자 이상 뭐 그런거 유효성검사하고 다시 응답해주고 그러는것같다.
 
-
+    def get_success_url(self):
+        return reverse('accountapp:detail',
+                       kwargs={'pk': self.object.user.pk}) # 여기서 self.object가 의미하는건 model=Profile 부분.
 
 @method_decorator(profile_ownership_required, 'get')
 @method_decorator(profile_ownership_required, 'post')
@@ -29,5 +30,8 @@ class ProfileUpdateView(UpdateView):
     model = Profile
     context_object_name = 'target_profile'
     form_class = ProfileCreationForm
-    success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'profileapp/update.html'
+
+    def get_success_url(self):
+        return reverse('accountapp:detail',
+                       kwargs={'pk': self.object.user.pk})
